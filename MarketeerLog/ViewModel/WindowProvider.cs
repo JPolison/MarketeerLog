cb1896515c57cd74818e6f47875aec020cb87b74
Polison <PolisonGit@gmail.com>
2021-06-19 23:13:34 -0400

    Added ways to edit the item listings

---
+++ MarketeerLog/ViewModel/WindowProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MarketeerLog.ViewModel
+{
+    public class WindowProvider : IWindowService
+    {
+        private static WindowProvider _instance;
+        public static WindowProvider Instance
+        {
+            get => _instance;
+            set => _instance = value;
+        }
+
+        public WindowProvider()
+        {
+            _instance = this;
+        }
+
+        public void ShowDialog<T>(IViewModel viewmodel) where T : Window, new()
+        {
+            Window win = new T();
+            win.DataContext = viewmodel;
+            win.ShowDialog();
+            
+        }
+    }
+}
